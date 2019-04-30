e1e6926da590a23cc6780b5a7c1a002bfd05ce57
Nicolas Cárcamo <nicolas.carcamo.f@usach.cl>
2019-04-30 14:19:15 -0400

    Modificaciones front

--- src/components/postActor.vue
+++ src/components/postActor.vue
@@ -4,7 +4,9 @@
     <input type="text" class="form-control" v-model="first_name" placeholder="Nombre">
     <br>
     <input type="text" class="form-control" v-model="last_name" placeholder="Apellido">
-    <button type="submit" @click="postActor">Crear</button>
+    <br>
+    <vs-button color="success" type="gradient" @click="postActor">Crear</vs-button>
+    
   </div>
 </template>
 
@@ -26,8 +28,9 @@
         axios.post('http://localhost:8081/actors/create',{
           first_name : this.first_name,
           last_name : this.last_name
-        }).
-        then(response=>{console.log(response.data)});
+        })
+        .then(response=>{console.log(response.data)
+        });
       }
     }
   }
