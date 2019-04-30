4a75ccde28eaab01b6de60942766340a49500720
benjamin <benjamin.munoz.tapia@gmail.com>
2019-04-30 11:50:05 -0400

    metodo post

--- src/components/postActor.vue
+++ src/components/postActor.vue
@@ -1,54 +1,33 @@
 <template>
-    <div class="columns" id="agregarActor">
-      <div class="column is-two-thirds">
-      <section class="section">
-        <h1 class="title">Agregar actor</h1>
-
-        <section class="form">
-          <div class="field">
-            <label class="label">First name</label>
-            <div class="control">
-              <input name="name" 
-                v-model="form.name"  
-                class="input" type="text" placeholder="First name">
-            </div> 
-          </div>
-
-          <div class="field">
-            <label class="label">Last name</label>
-            <div class="control">
-              <input name="name" 
-                v-model="form.name"  
-                class="input" type="text" placeholder="Last name">
-            </div> 
-          </div>
-        
-          
-          
-        </section>
-      </section>
-    </div>
+  <div class="agregarActor">
+    <h1>POST ACTOR</h1>
+    <input type="text" class="form-control" v-model="first_name" placeholder="Nombre">
+    <br>
+    <input type="text" class="form-control" v-model="last_name" placeholder="Apellido">
+    <button type="submit" @click="postActor">Crear</button>
   </div>
 </template>
 
 <script>
-  import {APIService} from '../APIService';
-  const API_URL = 'http://localhost:8081';
-  const apiService = new APIService();
+  import axios from 'axios';
 
   export default {
+    name : 'agregarActor',
     props: ['postId'],
 
     data() {
-      return {
-        firstName: '',
-        lastName: ''
-      }
+      return{
+        first_name:'',
+        last_name:''
+      };
     },
-
-    methods: {
-      onSubmit() {
-        apiService.createActor('actor', {actor: this.actor, id: this.postId}).then(post => this.$emit('completed', actor));
+    methods:{
+      postActor(){
+        axios.post('http://localhost:8081/actors/create',{
+          first_name : this.first_name,
+          last_name : this.last_name
+        }).
+        then(response=>{console.log(response.data)});
       }
     }
   }
